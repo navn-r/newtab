f9e4bf5dd84d7f51a1a08b36504346a4fb5d50ea
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-12-12 16:34:05 -0500

    do the images work now?

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ document.getElementById("inner").append(
     const button = document.createElement("div");
     const icon = document.createElement("img");
 
-    icon.src = `/img/${src}`;
+    icon.src = `./img/${src}`;
     icon.className = "icon";
 
     button.title = title;
